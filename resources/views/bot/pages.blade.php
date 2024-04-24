637e258413a4584b73b6e2d1e22b0f735ead3e3d
arabkhondev <starc9899@gmail.com>
2024-04-24 21:11:22 +0500

    change file

--- resources/views/bot/pages.blade.php
+++ resources/views/bot/pages.blade.php
@@ -1,3 +1,16 @@
 <div>
     <!-- Order your soul. Reduce your wants. - Augustine -->
+    <div class="container">
+        <div class="main">
+            <div class="top">
+                <div class="nav_links">
+                    <div class="link"> <a href="#">Home</a></div>
+                    <div class="link"> <a href="#">Home</a></div>
+                    <div class="link"> <a href="#">Home</a></div>
+                    <div class="link"> <a href="#">Home</a></div>
+                    <div class="link"> <a href="#">Home</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
